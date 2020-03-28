22092c22bed3e53170559772effbeadbd1eed7bb
Divy Bramhecha <42085970+TGDivy@users.noreply.github.com>
2020-03-28 16:47:03 +0000

    n square time for mst

--- tests.py
+++ tests.py
@@ -2,3 +2,19 @@ import math
 import graph
 
 
+if __name__ == '__main__':
+    g = graph.Graph(6,"sixnodes")
+    #g = graph.Graph(-1,"cities50")
+
+    print("Raw: %0.3f"%(g.tourValue()))
+    g.swapHeuristic()
+    print("Swapped: %0.3f"%g.tourValue())
+    g.TwoOptHeuristic()
+    print("TwoOpt %0.3f"%g.tourValue())
+    #graph = Graph(6,"sixnodes")
+    g.Greedy()
+    print("Greedy %0.3f"%g.tourValue())
+    g.Custom()
+    print("Custom %0.3f"%g.tourValue())
+
+
